04642b184b62e4d61a00a6969c5b306312fa5c1a
Njunco <nicolasjunco14@hotmail.com>
2024-05-25 14:59:43 -0500

    nuevos cambios

--- model/repository/service/TaskService.java
+++ model/repository/service/TaskService.java
@@ -39,4 +39,54 @@ public class TaskService {
         }
         return null;
     }
+
+    public Task updateDueDate(Long id, Date dueDate) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            Task updatedTask = task.get();
+            updatedTask.setDueDate(dueDate);
+            return taskRepository.save(updatedTask);
+        }
+        return null;
+    }
+
+    public Task assignUser(Long id, String assignedUser) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            Task updatedTask = task.get();
+            updatedTask.setAssignedUser(assignedUser);
+            return taskRepository.save(updatedTask);
+        }
+        return null;
+    }
+
+    public Task addTags(Long id, List<String> tags) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            Task updatedTask = task.get();
+            updatedTask.setTags(tags);
+            return taskRepository.save(updatedTask);
+        }
+        return null;
+    }
+
+    public Task setPriority(Long id, String priority) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            Task updatedTask = task.get();
+            updatedTask.setPriority(priority);
+            return taskRepository.save(updatedTask);
+        }
+        return null;
+    }
+
+    public Task addComments(Long id, List<String> comments) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            Task updatedTask = task.get();
+            updatedTask.setComments(comments);
+            return taskRepository.save(updatedTask);
+        }
+        return null;
+    }
 }
